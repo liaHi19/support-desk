933003f0c383b1df92bd45c57e7aea18e7e6893f
liaHi19 <gn.natalia19@gmail.com>
2022-12-02 11:35:45 +0200

    [#3] Add connection to database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 require("dotenv").config();
+require("colors");
 const { errorHandler } = require("./middlewares/errorMiddleware");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
+
+//Connect to Database
+connectDB();
 
 const app = express();
 
