ef71f48d88618fcb712bc415eb6dd50648042105
liaHi19 <gn.natalia19@gmail.com>
2022-12-03 17:20:52 +0200

    [#9] Add Home UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="heading">
+        <h1>What do you need to help with?</h1>
+        <p>Please choose from options below</p>
+      </section>
+      <Link to="/new-ticket" className="btn btn-reverse btn-block">
+        <FaQuestionCircle /> Create a New Ticket
+      </Link>
+      <Link to="/tickets" className="btn btn-block">
+        <FaTicketAlt /> View My Tickets
+      </Link>
+    </>
+  );
 };
 
 export default Home;
