19c8414f66da2d3864d0c36cad1d48e09bdcca32
liaHi19 <gn.natalia19@gmail.com>
2022-12-04 11:02:41 +0200

    [#12] Add routes for Tickets

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,8 @@ app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/tickets", require("./routes/ticketRoutes"));
+
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
