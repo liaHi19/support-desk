725f9a1a682f58218a6a10883e27c75844cb6695
liaHi19 <gn.natalia19@gmail.com>
2022-12-04 15:56:08 +0200

    [#15] Add useAuthStatus for protecting routes

---
+++ frontend/src/pages/NewTicket.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewTicket = () => {
+  return <div>new ticket</div>;
+};
+
+export default NewTicket;
--- frontend/src/router/MainRoute.jsx
+++ frontend/src/router/MainRoute.jsx
@@ -4,7 +4,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../components/Layout";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import NewTicket from "../pages/NewTicket";
 import Register from "../pages/Register";
+import PrivateRoute from "./PrivateRoute";
 
 const MainRoute = () => {
   return (
@@ -14,6 +16,9 @@ const MainRoute = () => {
           <Route index element={<Home />} />
           <Route path="register" element={<Register />} />
           <Route path="login" element={<Login />} />
+          <Route path="new-ticket" element={<PrivateRoute />} >
+            <Route index element={<NewTicket />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
